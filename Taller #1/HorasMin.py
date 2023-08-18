745331b7d1888e3ae67a6ef3aa472486917f98d0
Mariana Vélez <83511782+CONEJITTA@users.noreply.github.com>
2023-08-17 23:21:04 -0500

    Update HorasMin.py

--- Taller #1/HorasMin.py
+++ Taller #1/HorasMin.py
@@ -1,4 +1,4 @@
-def CalculoAnguloReloj(hrs, minutes):
+def CalculoAnguloReloj(hrs, minutes): 
     if hrs == 12:
         hrs = 0
     if minutes == 60:
@@ -6,6 +6,8 @@ def CalculoAnguloReloj(hrs, minutes):
 
     if hrs == 0 and minutes == 0:
         return 0
+
+  # Estas operaciones son O(1) porque toman tiempo constante independiente de los valores
     
     angulo = abs((30*hrs - (11 / 2) * minutes) % 360)
     
@@ -13,9 +15,16 @@ def CalculoAnguloReloj(hrs, minutes):
     
     return min(angulo, anguloAlReves)
 
+  # también son de complejidad O(1) ya que son cálculos en tiempo constante 
+
 # Ej
-hrs = int(input("Escriba la hora: "))
+hrs = int(input("Escriba la hora: ")) 
 minutes = int(input("Escriba los minutos: "))
 
-angulo = CalculoAnguloReloj(hrs, minutes)
+angulo = CalculoAnguloReloj(hrs, minutes) # O(1)
 print(f"El ángulo menor que va a estar entre las manecillas es de: {angulo} grados")
+
+# la ecuación asintótica es: T(hrs, minutes) = C, donde T es el tiempo que tarda la funcion en ejecutarse y c la constante, 
+# independientemente de la hora y los minutos ingresados, el programa tomará aproximadamente el mismo tiempo para calcular el ángulo.
+# por lo tanto, en términos de notación Big O, la función es O(1).
+# Hecho por Laura y Paulina
